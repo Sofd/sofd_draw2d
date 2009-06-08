6ea23b344b897ef72fa9607eb2918ace4055584c
Olaf Klischat <olaf.klischat@sofd.de>
2009-06-08 19:41:51 +0000

    + convenience constructors

--- src/de/sofd/draw2d/EllipseObject.java
+++ src/de/sofd/draw2d/EllipseObject.java
@@ -6,6 +6,15 @@ import java.awt.geom.Rectangle2D;
 
 public class EllipseObject extends DrawingObject {
 
+    public EllipseObject() {
+        super();
+    }
+    
+    public EllipseObject(double x1, double y1, double x2, double y2) {
+        super();
+        setLocation(x1, y1, x2, y2);
+    }
+    
     /**
      * 
      * @return the ellipse shape this EllipseObject represents
--- src/de/sofd/draw2d/RectangleObject.java
+++ src/de/sofd/draw2d/RectangleObject.java
@@ -2,4 +2,13 @@ package de.sofd.draw2d;
 
 public class RectangleObject extends DrawingObject {
 
+    public RectangleObject() {
+        super();
+    }
+    
+    public RectangleObject(double x1, double y1, double x2, double y2) {
+        super();
+        setLocation(x1, y1, x2, y2);
+    }
+    
 }
