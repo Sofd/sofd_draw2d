4fc4447957a66b390fa9c51b59e6f46a94d15b16
Olaf Klischat <olaf.klischat@sofd.de>
2009-06-16 23:58:08 +0000

    PolygonObject + accompanying viewer adapter (1st half-way functional implementation)

---
+++ src/de/sofd/draw2d/viewer/adapters/PolygonObjectViewerAdapter.java
@@ -0,0 +1,42 @@
+package de.sofd.draw2d.viewer.adapters;
+
+import java.awt.BasicStroke;
+import java.awt.Graphics2D;
+import java.awt.geom.Line2D;
+import java.awt.geom.Point2D;
+
+import de.sofd.draw2d.PolygonObject;
+import de.sofd.draw2d.viewer.DrawingViewer;
+
+public class PolygonObjectViewerAdapter extends DrawingObjectViewerAdapter {
+
+    public PolygonObjectViewerAdapter(DrawingViewer viewer, PolygonObject drawingObject) {
+        super(viewer, drawingObject);
+    }
+
+    @Override
+    public PolygonObject getDrawingObject() {
+        return (PolygonObject) super.getDrawingObject();
+    }
+    
+    @Override
+    public void paintObjectOn(Graphics2D g2d) {
+        g2d.setPaint(getDrawingObject().getColor());
+        g2d.transform(getViewer().getObjectToDisplayTransform());
+        g2d.setStroke(new BasicStroke(0));
+        int ptCount = getDrawingObject().getPointCount();
+        if (ptCount > 1) {
+            Point2D startPt = getDrawingObject().getPoint(0);
+            Point2D prevPt = (Point2D) startPt.clone();
+            for (int i = 1; i < ptCount; ++i) {
+                Point2D nextPt = getDrawingObject().getPoint(i);
+                g2d.draw(new Line2D.Double(prevPt, nextPt));
+                prevPt = nextPt;
+            }
+            if (getDrawingObject().isClosed()) {
+                g2d.draw(new Line2D.Double(prevPt, startPt));
+            }
+        }
+    }
+
+}
