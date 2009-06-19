96200644ac2b4c0b8171d7a79a805d91b336dd6e
Olaf Klischat <olaf.klischat@sofd.de>
2009-06-19 01:47:20 +0000

    cache ellipse shape on location changes

--- src/de/sofd/draw2d/EllipseObject.java
+++ src/de/sofd/draw2d/EllipseObject.java
@@ -6,6 +6,11 @@ import java.awt.geom.Rectangle2D;
 
 public class EllipseObject extends DrawingObject {
 
+    /**
+     * the ellipse shape this EllipseObject represents
+     */
+    private Ellipse2D ellipse = new Ellipse2D.Double();
+    
     public EllipseObject() {
         super();
     }
@@ -15,14 +20,19 @@ public class EllipseObject extends DrawingObject {
         setLocation(x1, y1, x2, y2);
     }
     
+    @Override
+    protected void onLocationChanged(Location oldLocation) {
+        super.onLocationChanged(oldLocation);
+        Rectangle2D bounds = this.getBounds2D();
+        this.ellipse = new Ellipse2D.Double(bounds.getX(), bounds.getY(), bounds.getWidth(), bounds.getHeight());
+    }
+    
     /**
      * 
      * @return the ellipse shape this EllipseObject represents
      */
     public Ellipse2D getEllipse() {
-        // TODO: cache the ellipse whenever this.location changes...
-        Rectangle2D bounds = this.getBounds2D();
-        return new Ellipse2D.Double(bounds.getX(), bounds.getY(), bounds.getWidth(), bounds.getHeight());
+        return ellipse;
     }
     
     @Override
